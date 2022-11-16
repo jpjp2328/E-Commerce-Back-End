221fa5cc216966a43a423c7881b263c1a5579431
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-11-17 05:36:45 +1100

    category routes, create/update routes added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,10 +38,32 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({ 
+    tag_name: req.body.category_name
+  })
+  .then (data => res.json(data))
+  .catch ((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {id: req.params.id}
+  })
+  .then(data => {
+    if (!data) {
+      res.status(404).json({message: 'No category with this id.'});
+      return;
+    }
+    res.json(data);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.delete('/:id', (req, res) => {
