b1f4b4783c19c89dc1e4689f91cbcae7f853d0e6
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-11-17 03:53:23 +1100

    product route, delete route added

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -114,6 +114,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+
+  Product.destroy({
+    where: {id:req.params.id},
+  })
+  .then(data => {
+    if (!data) {
+      res.status(404).json({message: 'No product with this id.'});
+      return;
+    }
+    res.json(data);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
