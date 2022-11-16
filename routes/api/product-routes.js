bef3cd34c41250e8940e1b40f310c5b9fed550eb
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-11-17 03:40:54 +1100

    product routes, get routes updated

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,35 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [{model:Category}, {model:Tag}]
+  })
+  .then (data => res.json(data))
+  .catch ((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {id: req.params.id},
+    include: [{model:Category}, {model:Tag}]
+  })
+  .then (data => {
+    if (!data){ 
+      res.status(404).json({message: 'No product with this id.'});
+      return;
+    }
+    res.json(data);
+  })
+  .catch ((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
