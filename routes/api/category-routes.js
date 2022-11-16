ec1c7f0015f21b7fa6872a16b8143317f84fb1c4
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-11-17 05:37:38 +1100

    category routes, delete route added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -68,6 +68,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {id: req.params.id}
+  })
+  .then(data => {
+    if (!data) {
+      res.status(404).json({message: 'No category with this id.'})
+      return;
+    }
+    res.json(data);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
